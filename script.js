92079b9b883055547478005001053dbd7e4925ad
noy flaysher <noyflaysher12@gmail.com>
2022-01-12 23:30:41 +0200

    changing color

--- script.js
+++ script.js
@@ -142,4 +142,29 @@ deleteButton.addEventListener('click',() => {
     calculator.updateDisplay();   
 });
 
+var one=document.getElementById('one');
+one.addEventListener('click', function(){
+    var body=document.querySelector('body');
+    var toggle=document.getElementById('circle');
+    body.classList.remove('active1');
+    body.classList.remove('active2');
+    toggle.style.left='0%';
+})
+var two=document.getElementById('two');
+two.addEventListener('click', function(){
+    var body=document.querySelector('body');
+    var toggle=document.getElementById('circle');
+    body.classList.add('active1');
+    body.classList.remove('active2');
+    toggle.style.left='40%';
+    
 
+})
+var three=document.getElementById('three');
+three.addEventListener('click', function(){
+    var body=document.querySelector('body');
+    var toggle=document.getElementById('circle');
+    body.classList.add('active2');
+    body.classList.remove('active1');
+    toggle.style.left='80%';
+})
